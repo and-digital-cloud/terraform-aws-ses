de128fc8c766fc0772ddfe5fb5f930adc254dbaf
Man-Wai Tse <man-waitse@Man-Wais-MBP.lan>
2021-09-06 17:30:42 +0100

    save point of today - lambda

--- monitoring-governance/inputs.tf
+++ monitoring-governance/inputs.tf
@@ -1,20 +1,24 @@
-variable "tags" {type = "map"}
+variable "tags" {
+  default     = {}
+  type        = map(any)
+  description = "Map of tags to assign to resources."
+}
 
 variable "aws_profile" {
-	default = ""
-	type = string
-	description = "value"
+  default     = ""
+  type        = string
+  description = "value"
 }
 
 variable "notification_email" {
-	default = ""
-	type = string
-	description = "value"
+  default     = ""
+  type        = string
+  description = "value"
 }
 
-variable "iam_path" {
-	default = ""
-	type = string
-	description = "value"
-}
+# variable "iam_path" {
+#   default     = ""
+#   type        = string
+#   description = "value"
+# }
 
