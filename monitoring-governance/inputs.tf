a083da4b1be80f7dc8b9b89cc0b6baa12ee6af3e
Man-Wai Tse <man-waitse@Man-Wais-MBP.lan>
2021-09-06 17:06:14 +0100

    ading types and empty description

--- monitoring-governance/inputs.tf
+++ monitoring-governance/inputs.tf
@@ -1,7 +1,20 @@
 variable "tags" {type = "map"}
 
-variable "aws_profile" {default = ""}
+variable "aws_profile" {
+	default = ""
+	type = string
+	description = "value"
+}
 
-variable "notification_email" {default = ""}
+variable "notification_email" {
+	default = ""
+	type = string
+	description = "value"
+}
 
-variable "iam_path" {default = ""}
+variable "iam_path" {
+	default = ""
+	type = string
+	description = "value"
+}
+
