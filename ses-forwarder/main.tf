8f191a3efedfbaceb70c45ebbd28fc5409471ced
Man-Wai Tse <man-waitse@Man-Wais-MBP.lan>
2021-09-08 17:27:44 +0100

    feat: adding private policy to s3 bucket

--- ses-forwarder/main.tf
+++ ses-forwarder/main.tf
@@ -99,6 +99,16 @@ resource "aws_s3_bucket" "bucket" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+
+}
+
 resource "aws_s3_bucket_policy" "lb_logs_access_policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.bucket_policy.json
